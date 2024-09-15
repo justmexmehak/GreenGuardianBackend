04966f9b6b61d71729f89176e32401869212d9e4
justmexmehak <mehaknauman6@gmail.com>
2024-09-15 22:36:45 +0500

    corsheader

--- greenguardian/settings.py
+++ greenguardian/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,10 +58,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'greenguardian.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Local development
+    "https://green-guardian-opal.vercel.app",  # Replace with the actual frontend domain
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
