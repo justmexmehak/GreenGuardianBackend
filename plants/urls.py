223fcad7031ee43ee3f52407bbb18080c9e84a7f
justmexmehak <mehaknauman6@gmail.com>
2024-09-15 17:52:24 +0500

    detail

--- plants/urls.py
+++ plants/urls.py
@@ -3,4 +3,5 @@ from .views import PlantCreateView
 
 urlpatterns = [
     path('create/', PlantCreateView.as_view(), name='plant-create'),
+    path('detail/', PlantDummyDataView.as_view(), name='plant-dummy-data'),
 ]
